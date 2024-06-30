b83f54d37481d5f44a36dcfec017dfb970f3e6f1
Frostyy23 <albertalpaka725@gmail.com>
2024-06-30 18:01:54 +0200

    Add files via upload

---
+++ shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Custom Money Printer"
+ENT.Author = "Frosty DO NOT COPY THIS ADDON"
+ENT.Category = "Custom Entities"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
